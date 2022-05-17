f7bf4ae32f3d2f7a3e0aa1ae7ea2190a81021aca
CORP\fumiya.ueno <fumiya.ueno@abejainc.com>
2022-05-17 17:17:44 +0900

    リクエスト時の複数ユーザー対応モック作成

--- ranking/predict.py
+++ ranking/predict.py
@@ -3,18 +3,29 @@ import numpy as np
 import json
 import traceback
 import time
+from io import BytesIO
 
 def handler(request, ctx):
 
     try:
-        json_data = open('./sample/ranking.json', 'r', encoding="utf-8")
-        json_data = json.load(json_data)
-        json_data['timestamp'] = int(time.time() * 1000)
+        ##リクエストの取得
+        r = request.read()
+        b = BytesIO(r)
+        j = json.loads(b.read().decode('utf-8'))
+
+        ret = {'result':[]}
+
+        for id in j['learner_id']:
+            json_data = open('./sample/ranking.json', 'r', encoding="utf-8")
+            json_data = json.load(json_data)
+            json_data['timestamp'] = int(time.time() * 1000)
+            json_data['personal']['id'] = id
+            ret['result'].append(json_data)
 
         return {
             'status_code': http.HTTPStatus.OK,
             'content_type': 'application/json; charset=utf8',
-            'content': json_data
+            'content': ret
         }
     except Exception as e:
         print(str(e))
